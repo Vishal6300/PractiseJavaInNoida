5c7a0428e89647e8ead878aaf1a796b83794db6d
Vishal6300 <vkdeoghar1998@gmail.com>
2023-05-22 19:10:38 +0530

    added daily leraning questions and answers

---
+++ CRUD_Operaton/src/myPackage/Employee.java
@@ -0,0 +1,51 @@
+package myPackage;
+
+public class Employee {
+		private  int empId=1234;
+		private String eName;
+		private int salary;
+		
+		
+		public int getEmpId() {
+			return empId;
+		}
+		public void setEmpId(int empId) {
+			this.empId = empId;
+		}
+		public String geteName() {
+			return eName;
+		}
+		public void seteName(String eName) {
+			this.eName = eName;
+		}
+		public int getSalary() {
+			return salary;
+		}
+		public void setSalary(int salary) {
+			this.salary = salary;
+		}
+		
+		public Employee(int empId, String eName, int salary) {
+			super();
+			this.empId = empId;
+			this.eName = eName;
+			this.salary = salary;
+		}
+		public Employee() {
+			
+		}
+		
+		@Override
+		public String toString() {
+			return "Employee [empId=" + empId + ", eName=" + eName + ", salary=" + salary + "]";
+		}
+		
+		
+		
+		public static void main(String[] args) {
+			Employee emp1= new Employee();
+//			emp1.getEmpId();
+			System.out.println(emp1.empId);
+		}
+		
+}
--- CRUD_Operaton/src/myPackage/Main.java
+++ CRUD_Operaton/src/myPackage/Main.java
@@ -8,8 +8,15 @@ public class Main {
 //		st.createTable();
 //		st.createData();
 //		st.updateData();
-		st.deleteData();
-		st.getAllDetails();
+//		st.deleteData();
+//		st.getAllDetails();
+		
+		Employee e1= new Employee();
+		e1.setEmpId(13206);
+		e1.seteName("sandy");
+		e1.setSalary(90000);
+		
+		System.out.println(e1.getEmpId()+" "+e1.geteName()+" "+e1.getSalary());
 	}
 
 }
