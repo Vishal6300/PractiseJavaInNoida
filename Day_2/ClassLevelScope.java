97b74df55334dec351820e7f3680be1b6f492372
Vishal6300 <vkdeoghar1998@gmail.com>
2023-06-07 19:56:10 +0530

    added daily leraning questions and answers

--- Day_2/ClassLevelScope.java
+++ Day_2/ClassLevelScope.java
@@ -7,11 +7,8 @@ public class ClassLevelScope {
     // This variables can be accessible everywhere in the class including in every
     // methods also.
 
-    public static void run() {
+    public void run() {
         System.out.println("Name is " + name + " and Roll number is" + rollNo);
     }
 
-    public static void main(String[] args) {
-        run();
-    }
 }
