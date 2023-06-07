b5d4e5a18d63499e0b3cf6dd6ae015d9d8d7efe6
Vishal6300 <vkdeoghar1998@gmail.com>
2023-06-07 19:55:16 +0530

    added daily leraning questions and answers

--- Day_2/ClassLevelScope.java
+++ Day_2/ClassLevelScope.java
@@ -1,6 +1,17 @@
 public class ClassLevelScope {
     // When a variable is declared inside a class and outside the methods, then
     // varibale is called as class level scope.
-    int rollNo=1; // class level scope
-    String name="Simbaa";  // class level scope
+    int rollNo = 1; // class level scope
+    String name = "Simbaa"; // class level scope
+
+    // This variables can be accessible everywhere in the class including in every
+    // methods also.
+
+    public static void run() {
+        System.out.println("Name is " + name + " and Roll number is" + rollNo);
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
 }
