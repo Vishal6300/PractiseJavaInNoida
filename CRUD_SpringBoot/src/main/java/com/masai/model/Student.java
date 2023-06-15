68a7bcf1b10b5949c81cda94f0d72b2d38faa2a7
Vishal6300 <vkdeoghar1998@gmail.com>
2023-06-15 12:56:17 +0530

    added CRUD operation project from SpringBoot

--- CRUD_SpringBoot/src/main/java/com/masai/model/Student.java
+++ CRUD_SpringBoot/src/main/java/com/masai/model/Student.java
@@ -7,5 +7,47 @@ public class Student {
 	private String city;
 	private int marks;
 	
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public Student(int rollNo, String name, String city, int marks) {
+		super();
+		this.rollNo = rollNo;
+		this.name = name;
+		this.city = city;
+		this.marks = marks;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 	
 }
