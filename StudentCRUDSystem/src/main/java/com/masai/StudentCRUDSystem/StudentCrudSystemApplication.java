b0fe29a63d85a03faa7bbba3df64c91fa7d03314
Vishal6300 <vkdeoghar1998@gmail.com>
2023-08-08 15:52:37 +0530

    added new project

---
+++ StudentCRUDSystem/src/main/java/com/masai/StudentCRUDSystem/StudentCrudSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masai.StudentCRUDSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudSystemApplication.class, args);
+	}
+
+}
---
+++ StudentCRUDSystem/src/test/java/com/masai/StudentCRUDSystem/StudentCrudSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.StudentCRUDSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
