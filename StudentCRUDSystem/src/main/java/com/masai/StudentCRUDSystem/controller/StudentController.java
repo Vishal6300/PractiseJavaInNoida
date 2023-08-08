88f40f23bef5f70a1fc25473f2b219798b368af1
Vishal6300 <vkdeoghar1998@gmail.com>
2023-08-08 21:34:40 +0530

    added implementation part

--- StudentCRUDSystem/src/main/java/com/masai/StudentCRUDSystem/controller/StudentController.java
+++ StudentCRUDSystem/src/main/java/com/masai/StudentCRUDSystem/controller/StudentController.java
@@ -1,17 +1,25 @@
 package com.masai.StudentCRUDSystem.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.StudentCRUDSystem.model.Student;
 import com.masai.StudentCRUDSystem.service.StudentService;
 
+import jakarta.transaction.Transactional;
+
 @RestController
+@RequestMapping("/stud")
 public class StudentController {
 	
 	@Autowired
@@ -22,7 +30,24 @@ public class StudentController {
 		return studentService.addStudent(student);
 	}
 	
+	@PutMapping("/updateStudent/{id}")
 	public Object updateStudent(@PathVariable Integer id, @RequestBody Student student){
 		return studentService.updateStudentById(id, student);
+	}
+	
+	@GetMapping("/viewAllStudent")
+	public List<Student> viewStudentList() {
+		return studentService.listOfAllStudent();
+	}
+	
+	@GetMapping("/getStudentById/{id}")
+	public Optional<Student> getStudentById(@PathVariable Integer id) {
+		return studentService.searchStudentByStudentId(id);
+	}
+	
+	@Transactional
+	@DeleteMapping("/deleteById/{id}")
+	public void deleteStudentById(@PathVariable Integer id) {
+		 studentService.deleteStudent(id);
 	}
 }
